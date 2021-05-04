5850d423edad9c01b25e1c2ba356e90da8cfdce1
Shane-Stewart <75265418+Shane-Stewart@users.noreply.github.com>
2021-05-04 11:21:18 -0500

    Update AssessmentTest.java

--- src/test/java/com/saucedemo/AssessmentTest.java
+++ src/test/java/com/saucedemo/AssessmentTest.java
@@ -22,9 +22,7 @@ public class AssessmentTest extends BaseTest {
      Your next test should:
         1. Interact with the menu on the left of the page and click "Input Forms"
         2. On the sub-menu, click "Checkbox Demo"
-        3. Under "Multiple Checkbox Demo" there are 2 bugs with the multiple checkbox component
-        and it is up to you to figure out what it is and write a test that fails at least 1/2 conditions
-        4. Hint: read the bulletpoints to help you find the bug(s)
+        3. Under "Multiple Checkbox Demo" validate the three requirments listed for the select all buttton text
 */
     @Test(enabled=false)
     public void checkboxTest() {
